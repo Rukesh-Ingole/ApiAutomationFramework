15cbd9e446abbb6983a418d6c714d0224ce460f8
RUKESH INGOLE <rukeshingole>
2022-06-23 17:42:58 +0530

    First Update

---
+++ APITesttingRestFramework/src/test/java/com/employee/testcases/TC007_Employee.java
@@ -0,0 +1,13 @@
+package com.employee.testcases;
+
+import org.testng.annotations.Test;
+
+import com.employee.base.TestBase;
+
+public class TC007_Employee extends TestBase{
+
+	@Test
+	public void Emplyee(){
+		logger.info("======TC007_Employee=====");
+	}
+}
